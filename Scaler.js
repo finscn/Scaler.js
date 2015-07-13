1723270e50822d489460cf9711569fd1ddf72d08
Rocky Wei <finscn@gmail.com>
2015-07-13 16:46:16 +0800

    refactor the code about NameSpace

--- Scaler.js
+++ Scaler.js
@@ -11,234 +11,242 @@
 
 "use strict";
 
-// 画面大小等比例缩放工具
-var Scaler = function(options) {
-    for (var p in options) {
-        this[p] = options[p];
-    }
-    this.init();
-};
+var GT = GT || {};
+
+(function(exports) {
+
+    // 画面大小等比例缩放工具
+    var Scaler = function(options) {
+        for (var p in options) {
+            this[p] = options[p];
+        }
+        this.init();
+    };
 
 
-// 设计稿不缩放, 在屏幕上居中
-Scaler.CENTER = 1;
+    // 设计稿不缩放, 在屏幕上居中
+    Scaler.CENTER = 1;
 
-// 让设计稿宽高都填满屏幕(画面内容可能会有剪切)
-Scaler.ASPECT_FILL = 2;
+    // 让设计稿宽高都填满屏幕(画面内容可能会有剪切)
+    Scaler.ASPECT_FILL = 2;
 
-// 让设计稿完整显示在屏幕上(画面四周可能会出现空白)
-Scaler.ASPECT_FIT = 3;
+    // 让设计稿完整显示在屏幕上(画面四周可能会出现空白)
+    Scaler.ASPECT_FIT = 3;
 
-// 让设计稿的宽度填满屏幕(画面内容可能会有剪切)
-Scaler.WIDTH_FIT = 4;
+    // 让设计稿的宽度填满屏幕(画面内容可能会有剪切)
+    Scaler.WIDTH_FIT = 4;
 
-// 让设计稿的高度填满屏幕(画面内容可能会有剪切)
-Scaler.HEIGHT_FIT = 5;
+    // 让设计稿的高度填满屏幕(画面内容可能会有剪切)
+    Scaler.HEIGHT_FIT = 5;
 
-// 改变设计稿的宽高比例,以适应屏幕
-Scaler.SCALE_FIT = 6;
+    // 改变设计稿的宽高比例,以适应屏幕
+    Scaler.SCALE_FIT = 6;
 
-Scaler.prototype = {
-    constructor: Scaler,
+    Scaler.prototype = {
+        constructor: Scaler,
 
-    //设计稿尺寸
-    width: null,
-    height: null,
+        //设计稿尺寸
+        width: null,
+        height: null,
 
-    // 屏幕尺寸 (实际的物理尺寸)
-    screenWidth: 0,
-    screenHeight: 0,
+        // 屏幕尺寸 (实际的物理尺寸)
+        screenWidth: 0,
+        screenHeight: 0,
 
-    // 缩放后的设计稿大小
-    scaledWidth: null,
-    scaledHeight: null,
+        // 缩放后的设计稿大小
+        scaledWidth: null,
+        scaledHeight: null,
 
-    // 屏幕可视区域的逻辑尺寸
-    fullWidth: 0,
-    fullHeight: 0,
+        // 屏幕可视区域的逻辑尺寸
+        fullWidth: 0,
+        fullHeight: 0,
 
-    // 设计稿在屏幕中的显示位置(按设计稿的单位)
-    offsetX: null,
-    offsetY: null,
+        // 设计稿在屏幕中的显示位置(按设计稿的单位)
+        offsetX: null,
+        offsetY: null,
 
-    // 缩放方式
-    scaleMode: Scaler.CENTER,
+        // 缩放方式
+        scaleMode: Scaler.CENTER,
 
-    // 缩放阈值 (需要缩放的比例过小时, 不进行缩放.)
-    scaleThreshold: 0.02,
+        // 缩放阈值 (需要缩放的比例过小时, 不进行缩放.)
+        scaleThreshold: 0.02,
 
-    // 缩放值
-    scale: null,
-    scaleX: null,
-    scaleY: null,
-    minScale: 0,
-    maxScale: Infinity,
+        // 缩放值
+        scale: null,
+        scaleX: null,
+        scaleY: null,
+        minScale: 0,
+        maxScale: Infinity,
 
-    init: function() {
-        this.screenWidth = this.screenWidth || this.width;
-        this.screenHeight = this.screenHeight || this.height;
-    },
+        init: function() {
+            this.screenWidth = this.screenWidth || this.width;
+            this.screenHeight = this.screenHeight || this.height;
+        },
 
-    setSize: function(w, h) {
-        this.width = w;
-        this.height = h;
-    },
-    setScreenSize: function(w, h) {
-        this.screenWidth = w;
-        this.screenHeight = h;
-    },
+        setSize: function(w, h) {
+            this.width = w;
+            this.height = h;
+        },
+        setScreenSize: function(w, h) {
+            this.screenWidth = w;
+            this.screenHeight = h;
+        },
 
-    setScaleMode: function(scaleMode) {
-        this.scaleMode = scaleMode;
-        this.update();
-    },
+        setScaleMode: function(scaleMode) {
+            this.scaleMode = scaleMode;
+            this.update();
+        },
 
-    // 在需要计算缩放数值时,调用此方法.
-    update: function() {
-        switch (this.scaleMode) {
-            case Scaler.ASPECT_FILL:
-                this.doAspectFill();
-                break;
-            case Scaler.ASPECT_FIT:
-                this.doAspectFit();
-                break;
-            case Scaler.WIDTH_FIT:
-                this.doWidthFit();
-                break;
-            case Scaler.HEIGHT_FIT:
+        // 在需要计算缩放数值时,调用此方法.
+        update: function() {
+            switch (this.scaleMode) {
+                case Scaler.ASPECT_FILL:
+                    this.doAspectFill();
+                    break;
+                case Scaler.ASPECT_FIT:
+                    this.doAspectFit();
+                    break;
+                case Scaler.WIDTH_FIT:
+                    this.doWidthFit();
+                    break;
+                case Scaler.HEIGHT_FIT:
+                    this.doHeightFit();
+                    break;
+                case Scaler.SCALE_FIT:
+                    this.doScaleFit();
+                    break;
+                case Scaler.CENTER:
+                default:
+                    this.doCenter();
+                    break;
+            }
+        },
+
+        ///////////////////////////////////////////
+        ////       开发者直接设置缩放比例         ////
+        ////  以下 3个 setter 方法通常不需要使用  ////
+        ///////////////////////////////////////////
+        setScale: function(scale) {
+            scale = Math.min(this.maxScale, Math.max(this.minScale, scale));
+            if (Math.abs(scale - 1) <= this.scaleThreshold) {
+                scale = 1;
+            }
+            this.setScaleX(scale);
+            this.setScaleY(scale);
+            this.scale = scale;
+        },
+        setScaleX: function(scaleX) {
+            this.scale = null;
+            this.scaleX = scaleX;
+            this.scaledWidth = this.width * this.scaleX;
+            this.fullWidth = Math.ceil(this.screenWidth / this.scaleX);
+            this.offsetX = (this.fullWidth - this.width) >> 1;
+            this.scaledOffsetX = (this.screenWidth - this.scaledWidth) >> 1;
+        },
+        setScaleY: function(scaleY) {
+            this.scale = null;
+            this.scaleY = scaleY;
+            this.scaledHeight = this.height * this.scaleY;
+            this.fullHeight = Math.ceil(this.screenHeight / this.scaleY);
+            this.offsetY = (this.fullHeight - this.height) >> 1;
+            this.scaledOffsetY = (this.screenHeight - this.scaledHeight) >> 1;
+        },
+
+
+        ///////////////////////////////////////////
+        ////  以下几个 do**** 方法通常不需要关心  ////
+        ///////////////////////////////////////////
+        doCenter: function() {
+            this.scaleX = this.scaleY = this.scale = 1;
+            this.updateScaleInfo();
+        },
+        doWidthFit: function() {
+            var scale = this.screenWidth / this.width;
+            scale = Math.min(this.maxScale, Math.max(this.minScale, scale));
+            if (Math.abs(scale - 1) <= this.scaleThreshold) {
+                scale = 1;
+            }
+            this.scaleX = this.scaleY = this.scale = scale;
+            this.updateScaleInfo();
+        },
+        doHeightFit: function() {
+            var scale = this.screenHeight / this.height;
+            scale = Math.min(this.maxScale, Math.max(this.minScale, scale));
+            if (Math.abs(scale - 1) <= this.scaleThreshold) {
+                scale = 1;
+            }
+            this.scaleX = this.scaleY = this.scale = scale;
+            this.updateScaleInfo();
+        },
+        doScaleFit: function() {
+            this.scaleX = Math.min(this.maxScale, Math.max(this.minScale, this.screenWidth / this.width));
+            this.scaleY = Math.min(this.maxScale, Math.max(this.minScale, this.screenHeight / this.height));
+            this.scale = null;
+            this.updateScaleInfo();
+        },
+        doAspectFill: function() {
+            var designR = this.width / this.height;
+            var screenR = this.screenWidth / this.screenHeight;
+            if (designR >= screenR) {
                 this.doHeightFit();
-                break;
-            case Scaler.SCALE_FIT:
-                this.doScaleFit();
-                break;
-            case Scaler.CENTER:
-            default:
-                this.doCenter();
-                break;
-        }
-    },
+            } else {
+                this.doWidthFit();
+            }
+        },
+        doAspectFit: function() {
+            var designR = this.width / this.height;
+            var screenR = this.screenWidth / this.screenHeight;
+            if (designR >= screenR) {
+                this.doWidthFit();
+            } else {
+                this.doHeightFit();
+            }
+        },
 
-    ///////////////////////////////////////////
-    ////       开发者直接设置缩放比例         ////
-    ////  以下 3个 setter 方法通常不需要使用  ////
-    ///////////////////////////////////////////
-    setScale: function(scale) {
-        scale = Math.min(this.maxScale, Math.max(this.minScale, scale));
-        if (Math.abs(scale - 1) <= this.scaleThreshold) {
-            scale = 1;
+        updateScaleInfo: function() {
+            this.scaledWidth = this.width * this.scaleX; // this.screenWidth ;
+            this.scaledHeight = this.height * this.scaleY; // this.screenHeight ;
+            this.fullWidth = Math.ceil(this.screenWidth / this.scaleX); // this.width;
+            this.fullHeight = Math.ceil(this.screenHeight / this.scaleY); // this.height;
+
+            this.offsetX = (this.fullWidth - this.width) >> 1; // 0 ;
+            this.offsetY = (this.fullHeight - this.height) >> 1; // 0 ;
+            this.scaledOffsetX = (this.screenWidth - this.scaledWidth) >> 1; // 0 ;
+            this.scaledOffsetY = (this.screenHeight - this.scaledHeight) >> 1; // 0 ;
         }
-        this.setScaleX(scale);
-        this.setScaleY(scale);
-        this.scale = scale;
-    },
-    setScaleX: function(scaleX) {
-        this.scale = null;
-        this.scaleX = scaleX;
-        this.scaledWidth = this.width * this.scaleX;
-        this.fullWidth = Math.ceil(this.screenWidth / this.scaleX);
-        this.offsetX = (this.fullWidth - this.width) >> 1;
-        this.scaledOffsetX = (this.screenWidth - this.scaledWidth) >> 1;
-    },
-    setScaleY: function(scaleY) {
-        this.scale = null;
-        this.scaleY = scaleY;
-        this.scaledHeight = this.height * this.scaleY;
-        this.fullHeight = Math.ceil(this.screenHeight / this.scaleY);
-        this.offsetY = (this.fullHeight - this.height) >> 1;
-        this.scaledOffsetY = (this.screenHeight - this.scaledHeight) >> 1;
-    },
+
+    };
 
 
-    ///////////////////////////////////////////
-    ////  以下几个 do**** 方法通常不需要关心  ////
-    ///////////////////////////////////////////
-    doCenter: function() {
-        this.scaleX = this.scaleY = this.scale = 1;
-        this.updateScaleInfo();
-    },
-    doWidthFit: function() {
-        var scale = this.screenWidth / this.width;
-        scale = Math.min(this.maxScale, Math.max(this.minScale, scale));
-        if (Math.abs(scale - 1) <= this.scaleThreshold) {
-            scale = 1;
-        }
-        this.scaleX = this.scaleY = this.scale = scale;
-        this.updateScaleInfo();
-    },
-    doHeightFit: function() {
-        var scale = this.screenHeight / this.height;
-        scale = Math.min(this.maxScale, Math.max(this.minScale, scale));
-        if (Math.abs(scale - 1) <= this.scaleThreshold) {
-            scale = 1;
-        }
-        this.scaleX = this.scaleY = this.scale = scale;
-        this.updateScaleInfo();
-    },
-    doScaleFit: function() {
-        this.scaleX = Math.min(this.maxScale, Math.max(this.minScale, this.screenWidth / this.width));
-        this.scaleY = Math.min(this.maxScale, Math.max(this.minScale, this.screenHeight / this.height));
-        this.scale = null;
-        this.updateScaleInfo();
-    },
-    doAspectFill: function() {
-        var designR = this.width / this.height;
-        var screenR = this.screenWidth / this.screenHeight;
-        if (designR >= screenR) {
-            this.doHeightFit();
+    ////////////////////////////////////////////
+    ////  以下为若干辅助函数, 非该工具核心功能  ////
+    ////////////////////////////////////////////
+
+    // 缩放canvas, 改变canvas的像素比例.
+    Scaler.prototype.resizeCanvas = function(canvas, useTransform) {
+        canvas.width = this.fullWidth;
+        canvas.height = this.fullHeight;
+        if (useTransform) {
+            this.resizeDom(canvas);
         } else {
-            this.doWidthFit();
+            canvas.style.width = this.screenWidth + "px";
+            canvas.style.height = this.screenHeight + "px";
         }
-    },
-    doAspectFit: function() {
-        var designR = this.width / this.height;
-        var screenR = this.screenWidth / this.screenHeight;
-        if (designR >= screenR) {
-            this.doWidthFit();
-        } else {
-            this.doHeightFit();
+    };
+
+    // 利用 CSS Transform 缩放dom元素, 其实这个意义不大.
+    Scaler.prototype.resizeDom = function(dom) {
+        dom.style.width = this.fullWidth + "px";
+        dom.style.height = this.fullHeight + "px";
+        if ("transform" in dom.style) {
+            dom.style.transformOrigin = "0 0";
+            dom.style.transform = "scale(" + this.scaleX + "," + this.scaleY + ")";
+        } else if ("webkitTransform" in dom.style) {
+            dom.style.webkitTransformOrigin = "0 0";
+            dom.style.webkitTransform = "scale(" + this.scaleX + "," + this.scaleY + ")";
         }
-    },
+    };
 
-    updateScaleInfo: function(){
-        this.scaledWidth = this.width * this.scaleX; // this.screenWidth ;
-        this.scaledHeight = this.height * this.scaleY; // this.screenHeight ;
-        this.fullWidth = Math.ceil(this.screenWidth / this.scaleX); // this.width;
-        this.fullHeight = Math.ceil(this.screenHeight / this.scaleY); // this.height;
+    exports.Scaler = Scaler;
 
-        this.offsetX = (this.fullWidth - this.width) >> 1; // 0 ;
-        this.offsetY = (this.fullHeight - this.height) >> 1; // 0 ;
-        this.scaledOffsetX = (this.screenWidth - this.scaledWidth) >> 1; // 0 ;
-        this.scaledOffsetY = (this.screenHeight - this.scaledHeight) >> 1; // 0 ;
-    }
-
-};
-
-
-////////////////////////////////////////////
-////  以下为若干辅助函数, 非该工具核心功能  ////
-////////////////////////////////////////////
-
-// 缩放canvas, 改变canvas的像素比例.
-Scaler.prototype.resizeCanvas = function(canvas, useTransform) {
-    canvas.width = this.fullWidth;
-    canvas.height = this.fullHeight;
-    if (useTransform) {
-        this.resizeDom(canvas);
-    } else {
-        canvas.style.width = this.screenWidth + "px";
-        canvas.style.height = this.screenHeight + "px";
-    }
-};
-
-// 利用 CSS Transform 缩放dom元素, 其实这个意义不大.
-Scaler.prototype.resizeDom = function(dom) {
-    dom.style.width = this.fullWidth + "px";
-    dom.style.height = this.fullHeight + "px";
-    if ("transform" in dom.style) {
-        dom.style.transformOrigin = "0 0";
-        dom.style.transform = "scale(" + this.scaleX + "," + this.scaleY + ")";
-    } else if ("webkitTransform" in dom.style) {
-        dom.style.webkitTransformOrigin = "0 0";
-        dom.style.webkitTransform = "scale(" + this.scaleX + "," + this.scaleY + ")";
-    }
-};
+}(GT));
